073f5c28c30adaf231afe45598b55b79db7d0794
EUN TAEK JEONG <jeong@oculavis.de>
2022-07-29 15:11:42 +0200

    test

--- app/src/main/java/com/mq/androidkotlinstudy/FirstFragment.kt
+++ app/src/main/java/com/mq/androidkotlinstudy/FirstFragment.kt
@@ -24,6 +24,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        //test
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
 
